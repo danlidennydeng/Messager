d23b0d8fdb0dc7e44d629d861b76d5df702d7501
Danli Deng <danlideng@yahoo.com>
2020-06-15 08:16:05 -0700

    change to github

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,9 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.action_cable.allowed_request_origins = ['https://127.0.0.1:6379',
+                                                 'http://127.0.0.1:6379']
+
+  config.action_cable.url = "wss://127.0.0.1:6379/cable"
 end
